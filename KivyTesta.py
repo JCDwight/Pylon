e6e11c913166d3503ef7f719e91605dcf81b75be
Jay Dwight <jcdwight@gmail.com>
2023-04-28 16:10:42 -0400

    sdfg

--- KivyTesta.py
+++ KivyTesta.py
@@ -311,14 +311,17 @@ class Monolith(App):
             if (self.scanLock == 0):
                 data = self.ReadSerial()
                 data = int(data, 2)
+                print('Printing User_Settings_DF: ')
+                print(self.user_settings_df)
                 for u in self.user_settings_df:
+                    print('Printing u: ')
                     print(u)
-                    if(data == u['ID']):
-                        self.CheckInScreen(u['Name'], "Images/" + u['P'], u['S'], u['C'])
-                        self.ser.write(b'3')
-                        Clock.schedule_once(partial(self.SplashScreen,self), 10)
-                        self.scanLock = 1
-                        print(self.user_settings_df)
+                    #if(data == u['ID']):
+                    #    self.CheckInScreen(u['Name'], "Images/" + u['P'], u['S'], u['C'])
+                    #    self.ser.write(b'3')
+                    #    Clock.schedule_once(partial(self.SplashScreen,self), 10)
+                    #    self.scanLock = 1
+                    #    print(self.user_settings_df)
             else:
                 self.ser.write(b'4')
                 self.PlaySound(57)
