7215c711e467a9fc53617342cbdebb372e4495cb
JCDwight <JCDwight@gmail.com>
2023-05-07 14:10:09 -0400

    k2

--- KivyTesta.py
+++ KivyTesta.py
@@ -146,7 +146,7 @@ class Monolith(App):
         self.add_user_settings('Chris',         '16878807',-1,'Default.png'     ,'Purple')
         self.add_user_settings('Coach Robert',  '10497089',-1,'Default.png'     ,'Blue')
         self.add_user_settings('Coach Charles', '50444699',-1,'Default.png'     ,'Cyan')
-        self.add_user_settings('Coach Kevin',   '44094159',-1,'Default.png'     ,'Cyan')
+        self.add_user_settings('Coach Kevin',   '44094159',-1,'Default.png'     ,'Green')
 
 
     def LoadSound(self):
