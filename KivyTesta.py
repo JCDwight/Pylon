6d58e0e76f3073f4a3558c772cc5ea7690a9c68a
JCDwight <jcdwight@gmail.com>
2023-07-02 14:36:14 -0400

    great

--- KivyTesta.py
+++ KivyTesta.py
@@ -52,8 +52,15 @@ def handle_client(conn):
             break
         print(f"Received data: {data.decode('utf-8')}")
         rdata = data
+        print(str(rdata))
         if rdata == b"update":
-            response = "1,RED"
+            rnd = random.randint(1,3)
+            if (rnd == 1):
+                response = "1,RED"
+            if (rnd == 3):
+                response = "1,GREEN"
+            if (rnd == 2):
+                response = "1,BLUE"
             conn.send(response.encode('utf-8'))
     conn.close()
 
