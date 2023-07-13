4a66a720e4bf7f42987dc1f3c441f53108e7dded
Jay Dwight <jcdwight@gmail.com>
2023-07-13 12:06:03 -0400

    dfgh

--- KivyTesta.py
+++ KivyTesta.py
@@ -339,8 +339,8 @@ class Monolith(App):
                     print("Exclude list: " + str(exclude))
                     print("Got right inside for exclude")
                     print("I: ",str(i))
-                    print(str(self.users_df.loc[i-1,'ID']))
-                    if (str(self.users_df.loc[i-1,'ID']) == str(j)): #Check if the checkin DB matches
+                    print(str(self.users_df.loc[i,'ID']))
+                    if (str(self.users_df.loc[i,'ID']) == str(j)): #Check if the checkin DB matches
                         print("Got right inside str(self.users_df.loc[i,'ID']) == str(j)")
 
                         pass                                       #an existing ID in the exclude list
@@ -350,14 +350,14 @@ class Monolith(App):
                         print("Got right inside else exclude")
 
                         for l in range(len(self.user_settings_df)):#Looks through settings DB to match the ID and find the MPIB ID
-                            if (str(self.user_settings_df.loc[l,'ID']) == str(self.users_df.loc[i-1,'ID'])):
+                            if (str(self.user_settings_df.loc[l,'ID']) == str(self.users_df.loc[i,'ID'])):
                                 temploc = self.user_settings_df.loc[l,'MPIB']
-                        if (self.users_df.loc[i-1,'CIOO'] == 1): #Check if in and assign color
+                        if (self.users_df.loc[i,'CIOO'] == 1): #Check if in and assign color
                             tempcolor = "GREEN"
-                        if (self.users_df.loc[i-1,'CIOO'] == 2): #Check if out and assign color
+                        if (self.users_df.loc[i,'CIOO'] == 2): #Check if out and assign color
                             tempcolor = "RED"
                         tempstr = tempstr + str(temploc) + "," + str(tempcolor) + "|"
-            exclude.append(str(self.users_df.loc[i-1,'ID']))
+            exclude.append(str(self.users_df.loc[i,'ID']))
 
         print("Got to before for loop")
         print("MPIB Status before MPIB_Status = tempstr :", str(self.MPIB_Status))
