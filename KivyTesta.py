3c1e19c630af22a3037bd8e08d272a74d4a742ea
JCDwight <jcdwight@gmail.com>
2023-07-22 15:05:28 -0400

    test

--- KivyTesta.py
+++ KivyTesta.py
@@ -548,7 +548,7 @@ class Monolith(App):
                         #print(str(self.users_df))
                         #self.Print_Checkins_With_Names()
                         self.CheckEveryoneOut()
-                        #self.PlaySound(78)
+                        self.PlaySound(78)
                     else:
                         self.ser.write(b'4')
                         #self.unauthorized_users_df = self.unauthorized_users_df.append({'ID': str(data), 'CIOT': datetime.datetime.now().strftime("%I:%M %p\n %B %d, %Y")})
