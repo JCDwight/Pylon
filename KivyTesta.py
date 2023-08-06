2e07abe9881a1feba49fb175d0fe11ef553363c7
JCDwight <jcdwight@gmail.com>
2023-08-06 15:21:29 -0400

    test2

--- KivyTesta.py
+++ KivyTesta.py
@@ -383,6 +383,7 @@ class Monolith(App):
     def CheckInScreen(self, name, imageFilePath, soundNum, color, ID, MPIB):
         if (CheckPlatform() == 1):
             self.ser.write(b'3')
+            time.sleep(50)
             self.ser.write(b'{color}')
         if (CheckPlatform() == 1):
             self.ser.flush()
