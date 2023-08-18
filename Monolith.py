57b2055d865414736dd50c550f6286fc778d3e70
JCDwight <jcdwight@gmail.com>
2023-08-18 17:29:28 -0400

    buggin out

--- Monolith.py
+++ Monolith.py
@@ -169,7 +169,6 @@ def Add_Checkinorout(checkin_df, ID):
     inorout = False
     
     for i in range(len(checkin_df)-1, -1, -1):
-        print("Got right before bug")
         if checkin_df.at[i, 'CIOO'] == True:
             inorout = False
             new_data = pd.DataFrame([{'ID': ID, 'CIOT': datetime.datetime.now().strftime("%I:%M:%S %p %B %d, %Y"), 'CIOO': False}])
