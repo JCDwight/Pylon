2392f87917bd46b832353e25ebc799156537ce2e
JCDwight <jcdwight@gmail.com>
2023-08-18 19:20:39 -0400

    sdf

--- Monolith.py
+++ Monolith.py
@@ -229,6 +229,7 @@ if __name__ == '__main__':
     while running:
         #Check Serial connection
         if (CheckPlatform() == 1):
+            print('Bits waiting: ', str(ser.in_waiting()))
             if (ser.inWaiting() > 10):
                 print(str(ser.inWaiting()))
                 time.sleep(0.1)
