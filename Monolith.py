b7afc849c0160bc0a8dc163fa1dc4002529c6504
JCDwight <jcdwight@gmail.com>
2023-08-18 21:30:50 -0400

    sdfg

--- Monolith.py
+++ Monolith.py
@@ -154,7 +154,7 @@ def ReadSerial(ser):
             return "0"  
         
 def CheckInScreen(screen, name, imageFilePath, soundNum, color, ID, MPIB):
-    display_text(screen,"Test Text",(100,100),36,RED)
+    display_text(screen,"Test Text",(100,100),36,RED,None)
     print('Got past display_text')
     pass    
 #Function to process any serial data we receive.  Should handle bad data/incomplete data
